24b4f07de606c452a91704e4de2b8ab2d6bec1aa
Steven <steven.camillieri@protonmail.com>
2022-02-17 05:13:44 -0500

    adding functionality

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div id="appWrap">
       <TopRow />
-      {/* <BottomRow /> */}
+      <BottomRow />
     </div>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -52,19 +52,48 @@ export default function Slider() {
   return (
     <main id="carouselContainer">
       <section id="photoSection">
-        <button id="prev"></button>
+        <button id="prev">
+          <svg
+            width="14"
+            height="24"
+            id="leftArrow"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13 0L1 12l12 12"
+              stroke="#FFF"
+              fill="none"
+              fill-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <Navbar id="nav" />
         <ul id="unorderedList">
           <li>
-            <img src={hero1d} className="photo" />
+            <img src={hero1d} className="photo" loading="lazy" />
           </li>
           <li>
-            <img src={hero2d} className="photo" />
+            <img src={hero2d} className="photo" loading="lazy" />
           </li>
           <li>
-            <img src={hero3d} className="photo" />
+            <img src={hero3d} className="photo" loading="lazy" />
           </li>
         </ul>
-        <button id="next"></button>
+        <button id="next">
+          <svg
+            width="14"
+            height="24"
+            id="rightArrow"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1  0l12 12L1 24"
+              stroke="#FFF"
+              fill="none"
+              fill-rule="evenodd"
+            />
+          </svg>
+        </button>
       </section>
       <section id="indicatorSection">
         <button class="indicator"></button>
